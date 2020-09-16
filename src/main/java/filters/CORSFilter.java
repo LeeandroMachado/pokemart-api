c1f1bbc84583d3e303116bad97afe3276437c19f
Leandro Machado <leandro.almachado@gmail.com>
2020-09-16 19:44:57 -0300

    ajuste do cors

--- src/main/java/filters/CORSFilter.java
+++ src/main/java/filters/CORSFilter.java
@@ -41,8 +41,7 @@ public class CORSFilter implements Filter {
     // Authorize (allow) all domains to consume the content
     ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
     ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
-    ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Credentials", "true");
-    ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "*");
 
     HttpServletResponse resp = (HttpServletResponse) servletResponse;
 
